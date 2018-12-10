e1bcc6f9b9a52f3d63e8d69ff39267113d69761c
dufufeisdu <fufei.du@gmail.com>
2018-12-10 10:07:15 -0800

    (update) Leecode

--- learn/Leecode_py/dynamic/least_cut.py
+++ learn/Leecode_py/dynamic/least_cut.py
@@ -4,7 +4,7 @@
 #  bbbbab least cut => bbb_bab          is 1
 
 
-# Solution(mine):
+# Solution(mine O(n!)):
 def least_cut(s):
     if len(s) < 2:
         return 0
@@ -14,6 +14,20 @@ def least_cut(s):
     for idx in range(1, len(s)):
         min_cut = min(min_cut, 1 + least_cut(s[0:idx]) + least_cut(s[idx:]))
     return min_cut
+
+
+# Solution(O(n^2))
+# Find the state of palindromes in s
+# if state[i][j] is True s[i:j+1] is palindrome
+# Procedure
+# for i in range(n-1,-1,-1):
+#     for j in range(i,n):
+#         if s[i]==s[j] and (i-j<2 ||s[i+1]==s[j-1]):
+#             state[i][j]=True
+
+
+def least_cut_prime(s):
+    pass
 
 
 if __name__ == "__main__":
---
+++ learn/Leecode_py/dynamic/max_area.py
@@ -0,0 +1,68 @@
+
+
+# this is an INCORRECT solution!!
+
+# from copy import deepcopy
+
+
+# def max_area(arr):
+#     area = 0
+
+#     wraped_arr = deepcopy(arr)
+#     header = [0 for i in range(len(wraped_arr) + 1)]
+#     wraped_arr = [[0] + ele for ele in wraped_arr]
+#     wraped_arr = [header] + wraped_arr
+#     print(wraped_arr)
+#     for i in range(1, len(wraped_arr)):
+#         for j in range(1, len(wraped_arr)):
+#             if wraped_arr[i][j] == 1:
+#                 wraped_arr[i][j] += max(wraped_arr[i - 1]
+#                                         [j], wraped_arr[i][j - 1])
+#                 area = max(area, wraped_arr[i][j])
+#     return area
+
+
+# correct:
+# L will record the left most consecutive start index
+# R will record the right most consecutive end index
+# H will record the height of the most consecutive 1 from the top
+# this three matrix contain everthing needed to compute the max area of the rectangle
+def max_area(arr):
+    if not arr:
+        return 0
+
+    m = len(arr)
+    n = len(arr[0])
+    H = [0] * n
+    L = [0] * n
+    R = [n] * n
+    ret = 0
+    for i in range(m):
+        left = 0
+        right = n
+        for j in range(n):
+            if arr[i][j] == 1:
+                H[j] += 1
+                L[j] = max(L[j], left)
+            else:
+                left = j + 1
+                H[j] = 0
+                L[j] = 0
+                R[j] = n
+        for k in range(n - 1, -1, -1):
+            if arr[i][k] == 1:
+                R[k] = min(R[k], right)
+                ret = max(ret, H[k] * (R[k] - L[k]))
+            else:
+                right = k
+    return ret
+
+
+if __name__ == "__main__":
+    test = [[1, 0, 1, 1, 1],
+            [1, 0, 1, 1, 0],
+            # [1, 0, 1, 1, 1],
+            # [1, 0, 1, 1, 0],
+            # [0, 0, 0, 1, 0]
+            ]
+    print(max_area(test))
