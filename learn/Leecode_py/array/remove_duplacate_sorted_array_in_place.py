61cadb452bbf3a09ed9252fb6f8c03a4123d223c
dufufeisdu <fufei.du@gmail.com>
2019-01-11 09:28:58 -0800

    (update)Leecode rotated array

--- learn/Leecode_py/array/remove_duplacate_sorted_array_in_place.py
+++ learn/Leecode_py/array/remove_duplacate_sorted_array_in_place.py
@@ -35,13 +35,25 @@ def remove_duplicate_more_than_twice(sorted_arr, max_duplicate=2):
 
     return sorted_arr[:j]
 
+# incorrect
+# def remove_duplicate_more_than_nth(sorted_arr, max_duplicate=2):
+#     n = len(sorted_arr)
+#     if n <= 2:
+#         return sorted_arr
+#     j = 2
+#     for i in range(2, n):
+#         if sorted_arr[j - 2] != sorted_arr[i]:
+#             sorted_arr[j] = sorted_arr[i]
+#             j += 1
+#     return sorted_arr[:j]
+
 
 class Test_Remove_Duplicate(unittest.TestCase):
     pass
 
 
 if __name__ == "__main__":
-    tests = [[], [1], [1, 1, 1], [1, 1, 2], [1, 1, 2, 2], [
+    tests = [[], [1], [1, 1, 1], [1, 2, 1, 1], [1, 1, 2, 2], [
         1, 1, 1, 1], [1, 1, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5]]
     I_result = [[], [1], [1], [1, 2], [1, 2], [1], [1, 2, 3, 4, 5]]
     II_result = [[], [1], [1, 1, 1], [1, 1, 2], [
@@ -49,6 +61,6 @@ if __name__ == "__main__":
     for idx, test in enumerate(tests):
         # unittest.TestCase().assertEqual(
         #     remove_duplicate(tests[i]), I_result[i], 'Incorrect')
-        # print("Problem One:", test, "--->", remove_duplicate(test))
-        print("Problme Two:", test, '--->',
-              remove_duplicate_more_than_twice(test))
+        print("Problem One:", test, "--->", remove_duplicate(test))
+        # print("Problme Two:", test, '--->',
+        #       remove_duplicate_more_than_nth(test))
